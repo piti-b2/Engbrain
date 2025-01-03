790fe64d09031fe6a234074688954067e34c9dd8
Piti-b2 <pitiphat.siri@gmail.com>
2025-01-04 00:35:42 +0700

    fix: update 202501040035

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -4,17 +4,17 @@ import {
   Toast,
   ToastClose,
   ToastDescription,
-  ToastProvider,
+  ToastProvider as ToastProviderOriginal,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
+} from "./toast"
+import { useToast } from "./use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
 
   return (
-    <ToastProvider>
+    <ToastProviderOriginal>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
@@ -30,6 +30,6 @@ export function Toaster() {
         )
       })}
       <ToastViewport />
-    </ToastProvider>
+    </ToastProviderOriginal>
   )
 }
