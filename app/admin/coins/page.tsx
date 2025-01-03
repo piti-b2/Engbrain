380c62f5f2a6c856f826faed738c6c11a23559c9
Piti-b2 <pitiphat.siri@gmail.com>
2025-01-03 14:13:28 +0700

    Update 202501031413

--- app/admin/coins/page.tsx
+++ app/admin/coins/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Card } from "@/components/ui/card";
+import { Card } from "../../../components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
